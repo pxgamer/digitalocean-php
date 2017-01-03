8d5e2af2c78e0bdf2a525c9b1ef19d7a1c89c545
PXgamer <owzie123@gmail.com>
2017-01-03 13:04:46 +0000

    Added tests for the other classes

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -1,11 +1,39 @@
 <?php
-use pxgamer\DigitalOcean\Client;
+
+use pxgamer\DigitalOcean;
 
 class MainTest extends PHPUnit_Framework_TestCase
 {
+    private $client;
+
     public function testCanBeInitialised()
     {
-        $digitalOcean = new Client;
-        $this->assertInstanceOf(Client::class, $digitalOcean);
+        $client = new DigitalOcean\Client();
+        $this->client = $client;
+        $this->assertInstanceOf(DigitalOcean\Client::class, $client);
+    }
+
+    public function testAccountCanBeInitialised()
+    {
+        $account = new DigitalOcean\Account($this->client);
+        $this->assertInstanceOf(DigitalOcean\Account::class, $account);
+    }
+
+    public function testDomainsCanBeInitialised()
+    {
+        $domains = new DigitalOcean\Domains($this->client);
+        $this->assertInstanceOf(DigitalOcean\Domains::class, $domains);
+    }
+
+    public function testDropletsCanBeInitialised()
+    {
+        $droplets = new DigitalOcean\Droplets($this->client);
+        $this->assertInstanceOf(DigitalOcean\Droplets::class, $droplets);
+    }
+
+    public function testDropletCanBeInitialised()
+    {
+        $droplet = new DigitalOcean\Droplet($this->client);
+        $this->assertInstanceOf(DigitalOcean\Droplet::class, $droplet);
     }
 }
