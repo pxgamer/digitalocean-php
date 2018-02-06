62df4749f202dd65af02df0feb0c7cd009a1a502
pxgamer <owzie123@gmail.com>
2018-02-06 22:59:24 +0000

    Add Client chaining methods

--- src/Client.php
+++ src/Client.php
@@ -12,8 +12,14 @@ namespace pxgamer\DigitalOcean;
  */
 class Client
 {
+    /**
+     * The API base URI.
+     */
     const BASE_URL = 'https://api.digitalocean.com/v2';
 
+    /**
+     * @var string
+     */
     public $authKey;
 
     /**
@@ -24,5 +30,45 @@ class Client
     public function setAuthKey(string $authKey)
     {
         $this->authKey = $authKey;
+    }
+
+    /**
+     *
+     */
+    public function account()
+    {
+        if (!($this->account instanceof Account)) {
+            $this->account = new Account($this->authKey);
+        }
+    }
+
+    /**
+     *
+     */
+    public function domains()
+    {
+        if (!($this->domains instanceof Domains)) {
+            $this->domains = new Domains($this->authKey);
+        }
+    }
+
+    /**
+     *
+     */
+    public function droplet()
+    {
+        if (!($this->droplet instanceof Droplet)) {
+            $this->droplet = new Droplet($this->authKey);
+        }
+    }
+
+    /**
+     *
+     */
+    public function droplets()
+    {
+        if (!($this->droplets instanceof Droplets)) {
+            $this->droplets = new Droplets($this->authKey);
+        }
     }
 }
