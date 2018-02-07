55724ccfff827337bb43253a7ac12cc471eb6033
pxgamer <owzie123@gmail.com>
2018-02-07 15:25:31 +0000

    Add additional tests for the Domains class

--- tests/DomainsTest.php
+++ tests/DomainsTest.php
@@ -26,4 +26,52 @@ class DomainsTest extends TestCase
 
         $this->assertInstanceOf(\stdClass::class, $response);
     }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanGetDomain()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], json_encode(new \stdClass())),
+        ]);
+
+        $handler = HandlerStack::create($mock);
+
+        $response = (new Domains($this->authKey, $handler))->getDomain('example.com');
+
+        $this->assertInstanceOf(\stdClass::class, $response);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanCreateDomain()
+    {
+        $mock = new MockHandler([
+            new Response(201, [], json_encode(new \stdClass())),
+        ]);
+
+        $handler = HandlerStack::create($mock);
+
+        $response = (new Domains($this->authKey, $handler))->createDomain('example.com');
+
+        $this->assertInstanceOf(\stdClass::class, $response);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testCanDeleteDomain()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], json_encode(new \stdClass())),
+        ]);
+
+        $handler = HandlerStack::create($mock);
+
+        $response = (new Domains($this->authKey, $handler))->deleteDomain('example.com');
+
+        $this->assertInstanceOf(\stdClass::class, $response);
+    }
 }
